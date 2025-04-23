722907636107311a38ddb7b2c9d0d214374d5727
lmcmz <lmcmze@gmail.com>
2025-04-23 23:39:48 +1000

    code clean up

--- Sources/Cadence/CadenceTargetType.swift
+++ Sources/Cadence/CadenceTargetType.swift
@@ -33,7 +33,10 @@ extension Flow {
     // Update execute function to use the specific return type
     public func query<T: Decodable>(_ target: CadenceTargetType,
                                     chainID: Flow.ChainID = .mainnet) async throws -> T {
-        let script = try decodeScript(from: target.cadenceBase64)
+        guard let data = Data(base64Encoded: target.cadenceBase64) else {
+            throw NSError(domain: "Invalid Cadence Base64 String", code: 9900001)
+        }
+        let script = Flow.Script(data: data)
         let api = Flow.FlowHTTPAPI(chainID: chainID)
         return try await api.executeScriptAtLatestBlock(script: script, arguments: target.arguments)
             .decode()
@@ -44,30 +47,13 @@ extension Flow {
                                 network: Flow.ChainID = .mainnet,
                                 @Flow.TransactionBuilder builder: () -> [Flow.TransactionBuild]
     ) async throws -> Flow.ID {
-        let script = try decodeScript(from: target.cadenceBase64)
+        guard let data = Data(base64Encoded: target.cadenceBase64) else {
+            throw NSError(domain: "Invalid Cadence Base64 String", code: 9900001)
+        }
         var tx = try await flow.buildTransaction(chainID: chainID, skipEmptyCheck: true, builder: builder)
-        tx.script = script
+        tx.script = .init(data: data)
         tx.arguments = target.arguments
         let signedTx = try await flow.signTransaction(unsignedTransaction: tx, signers: singers)
         return try await flow.sendTransaction(transaction: signedTx)
-    }
-    
-    private func decodeScript(from base64String: String) throws -> Flow.Script {
-        // First decode base64 to Data
-        guard let data = Data(base64Encoded: base64String) else {
-            throw NSError(domain: "Invalid Cadence Base64 String", code: 9900001)
-        }
-        
-        // Convert to string, preserving newlines
-        guard let scriptString = String(data: data, encoding: .utf8) else {
-            throw NSError(domain: "Invalid UTF8 encoding", code: 9900002)
-        }
-        
-        // Convert back to data with proper encoding
-        guard let scriptData = scriptString.data(using: .utf8) else {
-            throw NSError(domain: "Failed to encode script", code: 9900003)
-        }
-        
-        return .init(data: scriptData)
     }
 }
